e4e6b329e1fbb8c9f04018ff4c73584edcf09ab1
Leo <debonnetleo4@gmail.com>
2024-10-03 18:48:38 +0200

    suppression du role non prioritaire

--- src/functions/defineRoles.js
+++ src/functions/defineRoles.js
@@ -8,7 +8,6 @@ export const defineRoles = {
         const newPriorities = await db.query(`SELECT toplaner_priority, jungler_priority, midlaner_priority, botlaner_priority, support_priority FROM comptes WHERE discord_id = '${interaction.user.id}'`);
 
         let highPriorityMessage = "";
-        let lowPriorityMessage = "";
         let noPriorityMessage = "";
 
         priorityMessages(newPriorities[0].toplaner_priority, "toplaner");
@@ -18,29 +17,19 @@ export const defineRoles = {
         priorityMessages(newPriorities[0].support_priority, "support");
 
         function priorityMessages(priority, role) {
-            switch(priority) {
-                case "0":
-                    noPriorityMessage += (noPriorityMessage ? ", ": "") + role;
-                    break;
-                case "1":
-                    lowPriorityMessage += (lowPriorityMessage ? ", ": "") + role;
-                    break;
-                case "2":
-                    highPriorityMessage += (highPriorityMessage ? ", ": "") + role;
-                    break;
-            }
+            if(priority == 0) noPriorityMessage += (noPriorityMessage ? ", ": "") + role;
+            else highPriorityMessage += (highPriorityMessage ? ", ": "") + role;
         }
 
         // Build the embed
         const embed = new EmbedBuilder()
         .setTitle("Choix du/des roles")
-        .setDescription("Veuillez définir votre priorité de roles pour les inhouse.\nCliquez une fois sur un bouton pour le mettre en role secondaire, et une deuxième fois pour le mettre en role prioritaire.\n\nUne fois que les roles sont comme vous le souhaitez vous pouvez fermer tous les messages.")
+        .setDescription("Cliquez sur le bouton du role pour le mettre dans les joués ou non joués.\n\nUne fois que les roles sont comme vous le souhaitez vous pouvez fermer tous les messages.")
         .setColor(globals.embed.colorMain)
         .setTimestamp()
         .addFields(
-            { name: `Roles prioritaires`, value: (highPriorityMessage == "") ? " " : highPriorityMessage },
-            { name: `Roles secondaires`, value: (lowPriorityMessage == "") ? " " : lowPriorityMessage },
-            { name: `Roles non voulus`, value: (noPriorityMessage == "") ? " " : noPriorityMessage },
+            { name: `Roles joués`, value: (highPriorityMessage == "") ? " " : highPriorityMessage },
+            { name: `Roles non joué`, value: (noPriorityMessage == "") ? " " : noPriorityMessage },
         );
 
         const bTop = new ButtonBuilder()
@@ -91,23 +80,23 @@ export const defineRoles = {
 
         switch(role) {
             case "toplaner":
-                if(priorities[0].toplaner_priority < 2) await db.query(`UPDATE comptes SET toplaner_priority = ${parseInt(priorities[0].toplaner_priority) + 1} WHERE discord_id = '${interaction.user.id}'`);
+                if(priorities[0].toplaner_priority == 0) await db.query(`UPDATE comptes SET toplaner_priority = ${parseInt(priorities[0].toplaner_priority) + 1} WHERE discord_id = '${interaction.user.id}'`);
                 else await db.query(`UPDATE comptes SET toplaner_priority = 0 WHERE discord_id = '${interaction.user.id}'`);
                 break;
             case "jungler":
-                if(priorities[0].jungler_priority < 2) await db.query(`UPDATE comptes SET jungler_priority = ${parseInt(priorities[0].jungler_priority) + 1} WHERE discord_id = '${interaction.user.id}'`);
+                if(priorities[0].jungler_priority == 0) await db.query(`UPDATE comptes SET jungler_priority = ${parseInt(priorities[0].jungler_priority) + 1} WHERE discord_id = '${interaction.user.id}'`);
                 else await db.query(`UPDATE comptes SET jungler_priority = 0 WHERE discord_id = '${interaction.user.id}'`);
                 break;
             case "midlaner":
-                if(priorities[0].midlaner_priority < 2) await db.query(`UPDATE comptes SET midlaner_priority = ${parseInt(priorities[0].midlaner_priority) + 1} WHERE discord_id = '${interaction.user.id}'`);
+                if(priorities[0].midlaner_priority == 0) await db.query(`UPDATE comptes SET midlaner_priority = ${parseInt(priorities[0].midlaner_priority) + 1} WHERE discord_id = '${interaction.user.id}'`);
                 else await db.query(`UPDATE comptes SET midlaner_priority = 0 WHERE discord_id = '${interaction.user.id}'`);
                 break;
             case "botlaner":
-                if(priorities[0].botlaner_priority < 2) await db.query(`UPDATE comptes SET botlaner_priority = ${parseInt(priorities[0].botlaner_priority) + 1} WHERE discord_id = '${interaction.user.id}'`);
+                if(priorities[0].botlaner_priority == 0) await db.query(`UPDATE comptes SET botlaner_priority = ${parseInt(priorities[0].botlaner_priority) + 1} WHERE discord_id = '${interaction.user.id}'`);
                 else await db.query(`UPDATE comptes SET botlaner_priority = 0 WHERE discord_id = '${interaction.user.id}'`);
                 break;
             case "support":
-                if(priorities[0].support_priority < 2) await db.query(`UPDATE comptes SET support_priority = ${parseInt(priorities[0].support_priority) + 1} WHERE discord_id = '${interaction.user.id}'`);
+                if(priorities[0].support_priority == 0) await db.query(`UPDATE comptes SET support_priority = ${parseInt(priorities[0].support_priority) + 1} WHERE discord_id = '${interaction.user.id}'`);
                 else await db.query(`UPDATE comptes SET support_priority = 0 WHERE discord_id = '${interaction.user.id}'`);
                 break;
             default :
