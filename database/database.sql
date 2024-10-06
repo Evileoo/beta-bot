7571d1a84a023848cba88a9360fa04c63aa4c8d8
Leo <debonnetleo4@gmail.com>
2024-10-06 14:30:03 +0200

    update database

--- database/database.sql
+++ database/database.sql
@@ -11,16 +11,8 @@ DROP TABLE IF EXISTS comptes;
 
 -- comptes
 CREATE TABLE IF NOT EXISTS comptes (
-    id INT NOT NULL AUTO_INCREMENT COMMENT "Identifiant local du compte",
     discord_id VARCHAR(30) NOT NULL COMMENT "Identifiant discord du compte",
-    riot_puuid VARCHAR(100) NOT NULL COMMENT "Identifiant riot du compte",
-    toplaner_priority DECIMAL(1,0) NULL DEFAULT 0 COMMENT "Priorité définie pour jouer en toplane",
-    jungler_priority DECIMAL(1,0) NULL DEFAULT 0 COMMENT "Priorité définie pour jouer en jungle",
-    midlaner_priority DECIMAL(1,0) NULL DEFAULT 0 COMMENT "Priorité définie pour jouer en midlane",
-    botlaner_priority DECIMAL(1,0) NULL DEFAULT 0 COMMENT "Priorité définie pour jouer en botlane",
-    support_priority DECIMAL(1,0) NULL DEFAULT 0 COMMENT "Priorité définie pour jouer en support",
-    link_status VARCHAR(20) COMMENT "État de la création du compte",
-    PRIMARY KEY (id)
+    riot_puuid VARCHAR(100) NOT NULL COMMENT "Identifiant riot du compte"
 ) ENGINE=INNODB COMMENT="Synchronisation lol / discord enregistré dans le bot";
 
 -- inhouse_session
@@ -38,6 +30,11 @@ CREATE TABLE IF NOT EXISTS inhouse_session (
 
 -- inhouse_partcipants
 CREATE TABLE IF NOT EXISTS inhouse_partcipants (
-    compte_id INT NOT NULL COMMENT "Identifiant local du compte",
-    inhouse_id INT NOT NULL COMMENT "Identifiant de la session InHouse"
+    discord_id VARCHAR(30) NOT NULL COMMENT "Identifiant discord du compte",
+    inhouse_id INT NOT NULL COMMENT "Identifiant de la session InHouse",
+    toplaner_priority DECIMAL(1,0) NULL DEFAULT 0 COMMENT "Priorité définie pour jouer en toplane",
+    jungler_priority DECIMAL(1,0) NULL DEFAULT 0 COMMENT "Priorité définie pour jouer en jungle",
+    midlaner_priority DECIMAL(1,0) NULL DEFAULT 0 COMMENT "Priorité définie pour jouer en midlane",
+    botlaner_priority DECIMAL(1,0) NULL DEFAULT 0 COMMENT "Priorité définie pour jouer en botlane",
+    support_priority DECIMAL(1,0) NULL DEFAULT 0 COMMENT "Priorité définie pour jouer en support"
 ) ENGINE=INNODB COMMENT="Participation à un inhouse";
