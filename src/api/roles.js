875188bd777960834e6060f8bbc039300f179d4d
Leo <debonnetleo4@gmail.com>
2024-12-08 20:22:59 +0100

    màj checks api

--- src/api/roles.js
+++ src/api/roles.js
@@ -1,5 +1,6 @@
 import {  } from 'discord.js';
 import { globals } from '../globals.js';
+import { db } from '../connections/database.js';
 
 export async function apiGetRoles(client, params) {
 
@@ -13,11 +14,22 @@ export async function apiGetRoles(client, params) {
         }
     }
 
-    //if(!params.key != process.env.APIKEY) {
-    //    return {
-    //        message: `La clé d'API est incorrecte`
-    //    }
-    //}
+    const keyCheck = await db.query(`SELECT api_key_expiration FROM api_access WHERE api_key = '${params.key}'`);
+
+    if(keyCheck.length == 0 || keyCheck[0].api_key_expiration == null) {
+        return {
+            message: `Clé d'API fournie incorrecte`
+        }
+    }
+
+    const today = new Date();
+    const expiration = new Date(keyCheck[0].api_key_expiration);
+
+    if(today > expiration) {
+        return {
+            message: `Clé d'API expirée`
+        }
+    }
 
     if(!params.hasOwnProperty("serverID")) {
         return {
